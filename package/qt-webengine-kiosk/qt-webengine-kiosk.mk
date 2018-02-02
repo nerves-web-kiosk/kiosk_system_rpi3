56c6ff7fd00e3094c117fb6a67cc0b4fd0008847
Justin Schneck <jschneck@mac.com>
2018-02-02 17:36:11 -0500

    use qt-webengine-kiosk fork

--- package/qt-webengine-kiosk/qt-webengine-kiosk.mk
+++ package/qt-webengine-kiosk/qt-webengine-kiosk.mk
@@ -4,10 +4,12 @@
 #
 ################################################################################
 
-QT_WEBENGINE_KIOSK_VERSION = e10953325c2b86790b48b26aa52c0b6fd7430be5
-QT_WEBENGINE_KIOSK_SITE = https://github.com/mobileoverlord/qt-webengine-kiosk.git
+QT_WEBENGINE_KIOSK_VERSION = 40745158d2cde953b919c35281217f504a488468
+QT_WEBENGINE_KIOSK_SITE = https://github.com/mobileoverlord/qt-webengine-kiosk-1
 QT_WEBENGINE_KIOSK_SITE_METHOD = git
 QT_WEBENGINE_KIOSK_DEPENDENCIES = qt5webengine
+QT_WEBENGINE_KIOSK_LICENSE = LGPL-3.0
+QT_WEBENGINE_KIOSK_LICENSE_FILES = doc/lgpl.html
 
 define QT_WEBENGINE_KIOSK_CONFIGURE_CMDS
 	(cd $(@D); $(TARGET_MAKE_ENV) $(QT5_QMAKE) PREFIX=/usr)
@@ -18,9 +20,11 @@ define QT_WEBENGINE_KIOSK_BUILD_CMDS
 endef
 
 define QT_WEBENGINE_KIOSK_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) -f Makefile \
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/src -f Makefile.qt-webengine-kiosk \
 		INSTALL_ROOT=$(TARGET_DIR) \
 		install_target
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+		INSTALL_ROOT=$(TARGET_DIR)
 endef
 
 $(eval $(generic-package))
