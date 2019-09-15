cf5595f1c8d820f5feec70942fdcd07b2ff253f6
Frank Hunleth <fhunleth@troodon-software.com>
2019-09-15 09:47:36 -0400

    Bump to nerves_system_br 1.9.1/nerves 1.5.0

---
+++ example/config/target.exs
@@ -0,0 +1,70 @@
+use Mix.Config
+
+network_ssid = System.get_env("NERVES_NETWORK_SSID")
+network_psk = System.get_env("NERVES_NETWORK_PSK")
+
+network_iface = System.get_env("NERVES_NETWORK_IFACE") || "eth0"
+key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
+
+wlan_conf =
+  if network_iface == "wlan0" do
+    config :nerves_network, :default,
+      wlan0: [ssid: network_ssid, psk: network_psk, key_mgmt: key_mgmt]
+  else
+    config :nerves_network, :default, eth0: [ipv4_address_method: :dhcp]
+  end
+
+config :nerves_network,
+  iface: network_iface
+
+config :webengine_kiosk,
+  uid: "kiosk",
+  gid: "kiosk",
+  data_dir: "/root/kiosk",
+  fullscreen: true,
+  background_color: "black",
+  progress: true,
+  sounds: false
+
+# Authorize the device to receive firmware using your public key.
+# See https://hexdocs.pm/nerves_firmware_ssh/readme.html for more information
+# on configuring nerves_firmware_ssh.
+
+keys =
+  [
+    Path.join([System.user_home!(), ".ssh", "id_rsa.pub"]),
+    Path.join([System.user_home!(), ".ssh", "id_ecdsa.pub"]),
+    Path.join([System.user_home!(), ".ssh", "id_ed25519.pub"])
+  ]
+  |> Enum.filter(&File.exists?/1)
+
+if keys == [],
+  do:
+    Mix.raise("""
+    No SSH public keys found in ~/.ssh. An ssh authorized key is needed to
+    log into the Nerves device and update firmware on it using ssh.
+    See your project's config.exs for this error message.
+    """)
+
+config :nerves_firmware_ssh,
+  authorized_keys: Enum.map(keys, &File.read!/1)
+
+# Configure nerves_init_gadget.
+# See https://hexdocs.pm/nerves_init_gadget/readme.html for more information.
+
+# Setting the node_name will enable Erlang Distribution.
+# Only enable this for prod if you understand the risks.
+node_name = if Mix.env() != :prod, do: "test_app"
+
+config :nerves_init_gadget,
+  ifname: network_iface,
+  address_method: :dhcp,
+  mdns_domain: "kiosk.local",
+  node_name: node_name,
+  node_host: :mdns_domain
+
+# Import target specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+# Uncomment to use target specific configurations
+
+# import_config "#{Mix.target()}.exs"
