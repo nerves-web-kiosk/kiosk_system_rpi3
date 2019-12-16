4037cfe7f5e9fa1e88e7e18f7760600ee94d8f29
Justin Schneck <jschneck@mac.com>
2019-12-16 11:43:16 -0500

    nerves_system_br: bump to v1.10.0

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule KioskSystemRpi3.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.5.0", runtime: false},
-      {:nerves_system_br, "1.9.2", runtime: false},
+      {:nerves_system_br, "1.10.0", runtime: false},
       {:nerves_toolchain_arm_unknown_linux_gnueabihf, "1.2.0", runtime: false},
       {:nerves_system_linter, "~> 0.3.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
